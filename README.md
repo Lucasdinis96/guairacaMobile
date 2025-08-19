9b0cecf776d8fecd6b8b2b6445af63160f58c806
user <lucas.dinis1996@gmail.com>
2025-08-18 21:45:54 -0300

    feat: forms

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-Aula 04 - Widgets e navegação entre páginas
+Aula 05 - Formulários e entrada de dados
 
-app_routes - criar na mesma pasta que main.dart, no diretório lib
+operador ternário
+
+? verifica true
+?? verifica false
--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_guairaca/cadastro_page.dart';
 import 'package:flutter_guairaca/detalhe_page.dart';
 import 'package:flutter_guairaca/home_page.dart';
 
 class AppRoutes {
   static const home = '/';
   static const detalhes = '/detalhes';
+  static const cadastro = '/cadastro';
   
   static Map <String, WidgetBuilder> routes = {
     home: (context) => HomePage(),
-    detalhes: (context) => DetalhePage()
+    detalhes: (context) => DetalhePage(),
+    cadastro: (context) => CadastroPage()
   };
 }
